6a4b044ae73355c9df25e5d6576152e39f07f690
danilrey <danapavlov280@gmail.com>
2025-04-20 19:59:50 +0500

    javaStack

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,41 @@
-import Car.*;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        Car Toyota = new Car();
-        Toyota.getSpeedInfo();
-        Toyota.increaseSpeed();
-        Toyota.increaseSpeed();
-        Toyota.increaseSpeed();
-        Toyota.getSpeedInfo();
-        Toyota.color = "Blue";
-        System.out.println(Toyota.color);
-        Toyota.setRun(12500);
-        Toyota.getRun();
-        Toyota.decreaseSpeed();
-        Toyota.getSpeedInfo();
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String input = scanner.next();
+            System.out.println(isBalanced(input));
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (!isMatchingPair(top, c)) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    private static boolean isMatchingPair(char open, char close) {
+        return (open == '(' && close == ')') ||
+                (open == '{' && close == '}') ||
+                (open == '[' && close == ']');
     }
 }
